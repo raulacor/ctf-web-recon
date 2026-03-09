a2746922a30f7aee107239f09f6e4cedc42118c2
raulacor <raul.acorsi@gmail.com>
2026-03-09 20:57:42 -0300

    Add: Third level domain scannig; remmoved nmap temp

--- ctf.py
+++ ctf.py
@@ -1,6 +1,21 @@
 import os
+#pip install pynput
+from pynput.keyboard import Key, Controller 
+#https://pynput.readthedocs.io/en/latest/
+
+keyboard = Controller()
 
 target = input("Input target: ")
 
-os.system(f"nmap -A -p 80 {target} -v >> nmap.txt")
-os.system(f"smbclient -N -L {target}")
+os.system("mkdir thirdlevels")
+
+print("Running sublist3r. . .")
+os.system(f"sublist3r -d {target} -o final.txt")
+os.system(f"echo {target} >> final.txt")
+
+print("Compiling third-level domains. . .")
+os.system("cat final.txt | grep -Po '(\w+\.\)w+\.\w+)$' | sort -u >> third-level.txt")
+
+print("Gathering full third-level domains. . .")
+os.system("subfinder -dL third-level.txt -o thirdlevels/domain.txt")
+os.system("cat thirdlevels/domain.txt | sort -u >> final.txt")
